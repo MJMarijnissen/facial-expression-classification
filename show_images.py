5ce4afda45dd760b2654228efedf51d28351a7d7
MJMarijnissen <49721236+MJMarijnissen@users.noreply.github.com>
2019-04-18 12:14:17 +0200

    Update show_images.py

--- show_images.py
+++ show_images.py
@@ -2,13 +2,13 @@
 """
 Created on Sun Feb 24 11:28:01 2019
 
+Shows the images with the assigned emotion
+
 @author: Kubus
 """
 
 from __future__ import print_function, division
 from builtins import range, input
-# Note: you may need to update your version of future
-# sudo pip install -U future
 
 import numpy as np
 import matplotlib.pyplot as plt
